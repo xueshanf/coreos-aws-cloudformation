3f06fc9bb71a92b850bca0bdc0eadc44b875275f
Xueshan Feng <xueshan.feng@gmail.com>
2014-09-17 18:52:03 +0000

    fix SSH from address.

--- aws/create-stack.sh
+++ aws/create-stack.sh
@@ -2,23 +2,23 @@
 
 which aws > /dev/null 2>&1 || { echo "aws cli it's not installed.  Aborting."; exit 1; }
 
-# Default values
+# Default values. All can be replaced by command line arguments.
 SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
 STACK_NAME="CoreOS-test-$RANDOM"
-STACK_DISCOVERY_URL=`curl -s https://discovery.etcd.io/new`
+STACK_DISCOVERY_URL=$(curl -s https://discovery.etcd.io/new)
 STACK_INSTANCE_TYPE="m3.medium"
 STACK_CLUSTER_SIZE=3
 STACK_KEY_PAIR="coreoscluster01"
 STACK_REGION="us-west-2"
-STACK_SSHFROM=`curl -s ipecho.net/plain`
+STACK_SSHFROM=$(curl -s ipecho.net/plain)/32
 
 dryrun=0
 
 help(){
-    echo "create-stack [-s <stack-name>] -k <keypair-name> -t <discovery-toten-url> -r region -t <ec2-type> -n <ec2-number> -d "
+    echo "create-stack [-s <stack-name>] -k <keypair-name> -u <discovery-toten-url> -r region -t <ec2-type> -n <ec2-number> -d "
 }
 
-while getopts "s:k:t:r:n:dh" OPTION
+while getopts "s:k:u:r:t:n:dh" OPTION
 do
     case $OPTION in
         s)
@@ -30,7 +30,7 @@ do
         t)
           STACK_INSTANCE_TYPE=$OPTARG
           ;;
-        t)
+        u)
           STACK_DISCOVERY_URL=$OPTARG
           ;;
         r)
@@ -56,7 +56,7 @@ echo "Name: $STACK_NAME"
 echo "Discovery URL: $STACK_DISCOVERY_URL"
 echo "Instance Type x Cluster Size: $STACK_INSTANCE_TYPE x $STACK_CLUSTER_SIZE"
 echo "EC2 Key Pair: $STACK_KEY_PAIR"
-echo "SSH From: $STACK_SSHFROM/32" \
+echo "SSH From: $STACK_SSHFROM"
 
 [ $dryrun -eq 1 ] && { echo "Dryrun only."; exit 0; }
 
