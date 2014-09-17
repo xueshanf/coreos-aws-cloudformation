5b010bb6dea99474d65eb73e3fe21236f87dfba4
Xueshan Feng <xueshan.feng@gmail.com>
2014-09-17 18:25:57 +0000

    added registry service.

---
+++ coreos/list_units.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z "$FLEETCTL_TUNNEL" ]; then
+    echo
+    echo "You must set FLEETCTL_TUNNEL (a resolvable address to one of your CoreOS instances)"
+    echo "e.g.:"
+    echo "export FLEETCTL_TUNNEL=1.2.3.4"
+    echo
+    exit 1
+fi
+
+SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
+cd $SCRIPT_PATH/../units
+
+# Add the service templates to Fleet
+fleetctl submit registry/registry@.service
+# Start instantiated units from the templates (+ a number)
+fleetctl start registry/registry@1.service
